d0cfe7ada6b12a49b881d63453a6c0ffc43b4fe3
zst123 <manzel.seet@gmail.com>
2014-05-20 22:19:46 +0800

    Add new swipe preference layout (1)
    add the main code

--- src/com/zst/app/multiwindowsidebar/MainActivity.java
+++ src/com/zst/app/multiwindowsidebar/MainActivity.java
@@ -8,20 +8,71 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.app.Activity;
-import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 
 public class MainActivity extends Activity {
 		
+	public enum PAGES {
+		MAIN(0),
+		APPEARENCE(1);
+
+		final int position;
+		PAGES(int pos) {
+			position = pos;
+		}
+	};
+	
+	ViewPager mViewPager;
+	PageAdapter mPageAdapter;
+	
+	// Thanks: http://just-another-blog.net/programming/how-to-implement-horizontal-view-swiping-with-tabs/
+	// Thanks: http://stackoverflow.com/questions/15845632/adding-preferencefragment-to-fragmentpageradapter
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);		
+		setContentView(R.layout.activity_viewpager);
 		// Display the fragment as the main content.
-		FragmentTransaction transaction = getFragmentManager().beginTransaction();
-		MainPreference fragment = new MainPreference();
-		transaction.replace(android.R.id.content, fragment);
-		transaction.commit();
+		
+		mPageAdapter = new PageAdapter(getFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mPageAdapter);
+	}
+	
+	public class PageAdapter extends FragmentPagerAdapter {
+		public PageAdapter(FragmentManager fm) {
+			super(fm);
+			
+		}
+		
+		@Override
+		public Fragment getItem(int position) {
+			if (position == PAGES.MAIN.position) {
+				return new MainPreference();
+			} else if (position == PAGES.APPEARENCE.position) {
+				
+			}
+			return new Fragment();
+		}
+		
+		@Override
+		public int getCount() {
+			return PAGES.values().length;
+		}
+		
+		@Override
+		public CharSequence getPageTitle(int position) {
+			if (position == PAGES.MAIN.position) {
+				return getString(R.string.pref_main_header);
+			} else if (position == PAGES.APPEARENCE.position) {
+				return getString(R.string.pref_appearance_header);
+			}
+			return "?";
+		}
 	}
 	
 	public static class MainPreference extends PreferenceFragment implements
